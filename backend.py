b39e40a9ec28d5b2c96b415a34fbee1ed2f52e23
pavan1588 <pavanagnihotri82@gmail.com>
2025-07-14 13:51:25 +0530

    Update backend.py

--- backend.py
+++ backend.py
@@ -55,7 +55,7 @@ init_firebase()
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[os.getenv('FRONTEND_URL')],
+    allow_origins=[os.getenv('www.scenecraft-ai.com')],
     allow_credentials=True,
     allow_methods=['*'],
     allow_headers=['*'],
